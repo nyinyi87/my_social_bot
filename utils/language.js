3900a17352cecdc64210f37f2e4b2de29aa8612c
nyinyi87 <nynyi2098@gmail.com>
2026-09-03 00:18:04 +0630

    Create language.js

---
+++ utils/language.js
@@ -0,0 +1,20 @@
+const language = {};
+
+function setLanguage(chatId, lang) {
+  language[chatId] = lang;
+}
+
+function getLanguage(chatId) {
+  return language[chatId] || "mm";
+}
+
+function text(chatId, mm, en) {
+  return getLanguage(chatId) === "mm" ? mm : en;
+}
+
+module.exports = {
+  setLanguage,
+  getLanguage,
+  text
+};
+
