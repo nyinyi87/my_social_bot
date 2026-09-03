7bdbed3e740db51503c1a2b870eb2b27a152ebd7
nyinyi87 <nynyi2098@gmail.com>
2026-09-04 00:26:20 +0630

    Update language.js

--- utils/language.js
+++ utils/language.js
@@ -1,19 +1,14 @@
 const language = {};
 
 function setLanguage(chatId, lang) {
-    language[chatId] = lang;
+  language[chatId] = lang;
 }
 
 function getLanguage(chatId) {
-    return language[chatId] || "mm";
-}
-
-function text(chatId, mm, en) {
-    return getLanguage(chatId) === "mm" ? mm : en;
+  return language[chatId] || "mm";
 }
 
 module.exports = {
-    setLanguage,
-    getLanguage,
-    text
+  setLanguage,
+  getLanguage
 };
