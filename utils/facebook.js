81e6777d334b4abdc921c6268096ea2a872984fe
nyinyi87 <nynyi2098@gmail.com>
2026-09-03 11:27:05 +0630

    Update facebook.js

--- utils/facebook.js
+++ utils/facebook.js
@@ -1,13 +1,23 @@
-const YTDLP = require("yt-dlp-wrap").default;
+const YTDlpWrap = require("yt-dlp-wrap").default;
 const path = require("path");
-const yt = new YTDLP();
+const fs = require("fs-extra");
+
+const yt = new YTDlpWrap();
+const DOWNLOAD_DIR = "./downloads";
+
+fs.ensureDirSync(DOWNLOAD_DIR);
 
 async function downloadFacebook(url) {
-  const outputPath = path.join(__dirname, `../downloads/fb_${Date.now()}.mp4`);
-  await yt.execPromise([url, "-f", "mp4", "-o", outputPath]);
-  return outputPath;
+  const file = `${Date.now()}_facebook.mp4`;
+  const output = path.join(DOWNLOAD_DIR, file);
+
+  await yt.execPromise([
+    url,
+    "-f", "best",
+    "-o", output
+  ]);
+
+  return output;
 }
 
-module.exports = {
-  downloadFacebook
-};
+module.exports = { downloadFacebook };
