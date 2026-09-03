739d3223fd5e5069d7e677b32a1cc44b57a057a2
nyinyi87 <nynyi2098@gmail.com>
2026-09-03 23:50:26 +0630

    Update youtube.js

--- utils/youtube.js
+++ utils/youtube.js
@@ -1,52 +1,36 @@
 const YTDlpWrap = require("yt-dlp-wrap").default;
-const fs = require("fs-extra");
-const path = require("path");
 
-const yt = new YTDlpWrap();
+const yt = new YTDlpWrap("/usr/local/bin/yt-dlp");
 
-const DOWNLOAD_DIR = "./downloads";
-
-fs.ensureDirSync(DOWNLOAD_DIR);
-
-// Video Download
 async function downloadVideo(url, quality) {
-
-  const file = `${Date.now()}_${quality}.mp4`;
-  const output = path.join(DOWNLOAD_DIR, file);
-
-  let format = "bestvideo+bestaudio";
-
-  if (quality !== "1080") {
-    format = `bestvideo[height<=${quality}]+bestaudio/best[height<=${quality}]`;
-  }
+  const output = `downloads/${Date.now()}.mp4`;
 
   await yt.execPromise([
     url,
-    "-f", format,
-    "--merge-output-format", "mp4",
-    "-o", output
+    "-f",
+    `bestvideo[height<=${quality}]+bestaudio/best[height<=${quality}]`,
+    "--merge-output-format",
+    "mp4",
+    "-o",
+    output
   ]);
 
   return output;
 }
 
-// MP3 Download
 async function downloadMP3(url) {
-
-  const file = `${Date.now()}.mp3`;
-  const output = path.join(DOWNLOAD_DIR, file);
+  const output = `downloads/${Date.now()}.mp3`;
 
   await yt.execPromise([
     url,
     "-x",
-    "--audio-format", "mp3",
-    "-o", output
+    "--audio-format",
+    "mp3",
+    "-o",
+    output
   ]);
 
   return output;
 }
 
-module.exports = {
-  downloadVideo,
-  downloadMP3
-};
+module.exports = { downloadVideo, downloadMP3 };
