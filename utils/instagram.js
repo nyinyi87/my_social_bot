f9348c1d62c37d6f55f9f56e162287af20c5df6a
nyinyi87 <nynyi2098@gmail.com>
2026-09-03 11:27:54 +0630

    Update instagram.js

--- utils/instagram.js
+++ utils/instagram.js
@@ -1,14 +1,22 @@
-const YTDLP = require("yt-dlp-wrap").default;
+const YTDlpWrap = require("yt-dlp-wrap").default;
 const path = require("path");
-const yt = new YTDLP();
+const fs = require("fs-extra");
+
+const yt = new YTDlpWrap();
+const DOWNLOAD_DIR = "./downloads";
+
+fs.ensureDirSync(DOWNLOAD_DIR);
 
 async function downloadInstagram(url) {
-  const outputPath = path.join(__dirname, `../downloads/insta_${Date.now()}.mp4`);
-  await yt.execPromise([url, "-o", outputPath]);
-  return outputPath;
+  const file = `${Date.now()}_instagram.mp4`;
+  const output = path.join(DOWNLOAD_DIR, file);
+
+  await yt.execPromise([
+    url,
+    "-o", output
+  ]);
+
+  return output;
 }
 
-module.exports = {
-  downloadInstagram
-};
-
+module.exports = { downloadInstagram };
