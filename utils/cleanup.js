d142f3c5edcb172b7132fb41ea3717bec22e8bba
nyinyi87 <nynyi2098@gmail.com>
2026-09-04 00:20:25 +0630

    Update cleanup.js

--- utils/cleanup.js
+++ utils/cleanup.js
@@ -1,19 +1,24 @@
 const cron = require("node-cron");
 const fs = require("fs");
+const path = require("path");
 
-cron.schedule("0 * * * *", () => {
+cron.schedule("*/10 * * * *", () => {
 
   if (!fs.existsSync("./downloads")) return;
 
   const files = fs.readdirSync("./downloads");
 
   files.forEach(file => {
-    const path = `./downloads/${file}`;
-    const age = Date.now() - fs.statSync(path).mtimeMs;
+
+    const filePath = path.join("./downloads", file);
+
+    const age =
+      Date.now() - fs.statSync(filePath).mtimeMs;
 
     if (age > 3600000) {
-      fs.unlinkSync(path);
+      fs.unlinkSync(filePath);
     }
+
   });
 
 });
