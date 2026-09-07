18411bddc34cf19fdfb0428e29917817d4394bca
nyinyi87 <nynyi2098@gmail.com>
2026-09-07 13:23:36 +0630

    Update cleanup.js

--- utils/cleanup.js
+++ utils/cleanup.js
@@ -2,9 +2,13 @@ const fs = require("fs");
 
 function autoDelete(filePath) {
   setTimeout(() => {
-    if (fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-      console.log("Deleted:", filePath);
+    try {
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        console.log("Deleted:", filePath);
+      }
+    } catch (err) {
+      console.log(err);
     }
   }, 60 * 60 * 1000); // 1 hour
 }
