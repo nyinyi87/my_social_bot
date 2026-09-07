d2ffc4e12fda577d896d647cbc9d39f794f1a618
nyinyi87 <nynyi2098@gmail.com>
2026-09-07 13:24:12 +0630

    Update instagram.js

--- utils/instagram.js
+++ utils/instagram.js
@@ -1,21 +1,58 @@
-const youtubedlp = require("yt-dlp-exec");
+const ytdlp = require("yt-dlp-exec");
 const path = require("path");
 const fs = require("fs-extra");
 
 const DOWNLOAD_DIR = path.join(__dirname, "../downloads");
 fs.ensureDirSync(DOWNLOAD_DIR);
 
-async function downloadInstagram(url) {
-  const fileName = `ig_${Date.now()}.mp4`;
+// Instagram Video/Reel
+async function downloadInstagram(url, quality = "720") {
+  const fileName = `ig_${quality}_${Date.now()}.mp4`;
   const output = path.join(DOWNLOAD_DIR, fileName);
 
-  await youtubedlp(url, {
-    format: "best",
+  await ytdlp(url, {
+    format: `best[height<=${quality}]`,
     output
   });
 
   const size = fs.statSync(output).size;
-  return { file: output, size, fileName };
+  return { file: output, fileName, size };
 }
 
-module.exports = { downloadInstagram };
+// Instagram MP3
+async function downloadInstagramMP3(url) {
+  const fileName = `ig_${Date.now()}.mp3`;
+  const output = path.join(DOWNLOAD_DIR, fileName);
+
+  await ytdlp(url, {
+    extractAudio: true,
+    audioFormat: "mp3",
+    output
+  });
+
+  const size = fs.statSync(output).size;
+  return { file: output, fileName, size };
+}
+
+// Instagram Photo / Carousel
+async function downloadInstagramPhoto(url) {
+  const info = await ytdlp(url, {
+    dumpSingleJson: true
+  });
+
+  let photos = [];
+
+  if (info.entries) {
+    photos = info.entries.map(e => e.url || e.thumbnail);
+  } else {
+    photos.push(info.thumbnail);
+  }
+
+  return { photos };
+}
+
+module.exports = {
+  downloadInstagram,
+  downloadInstagramMP3,
+  downloadInstagramPhoto
+};
